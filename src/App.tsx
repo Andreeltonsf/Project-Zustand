9ba3d7afc048fd6c43d7d304ae3614d223113ee4
Francisco Ferreira <franciscoandreeltonsf@gmail.com>
2025-10-03 14:31:21 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="justify-center items-center flex min-h-screen">
+      <Button>Button</Button>
+    </div>
+  );
+}
+
+export default App;
