939a8ca1ec5cab618de7b48d15b6fcf405f39bd6
Francisco Ferreira <franciscoandreeltonsf@gmail.com>
2025-10-06 14:17:32 -0300

    Irei forçar o update(É uma aplicação bem em baixo)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
+import { useEffect, useReducer } from "react";
+import { create } from "zustand";
 import { Button } from "./components/ui/button";
 
+const store = create((set, get) => ({
+  counter: 0,
+  //Increment deve ser imutável
+  increment: () => set((prevState) => ({ counter: prevState.counter + 1 })),
+}));
+
 function App() {
+  //Irei forçar o update quando o state mudar
+  const forceUpdate = useReducer((c) => c + 1, 0)[1];
+
+  useEffect(() => {
+    const unsubcribe = store.subscribe((newState, oldState) => {
+      forceUpdate();
+    });
+
+    return () => {
+      unsubcribe();
+    };
+  });
   return (
     <div className="justify-center items-center flex min-h-screen">
-      <Button>Button</Button>
+      <h1>Counter: {store.getState().counter}</h1>
+      <Button type="button" onClick={store.getState().increment()}>
+        Hello World
+      </Button>
     </div>
   );
 }
