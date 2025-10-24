61aa84623e0b681656c630832891e0b54802e7a6
Francisco Ferreira <franciscoandreeltonsf@gmail.com>
2025-10-24 19:18:10 -0300

    refactor: update Zustand store to use immer middleware for state management

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,13 @@
-import { create } from "zustand";
-import { createJSONStorage, devtools } from "zustand/middleware";
+import create from "zustand";
+import { devtools } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 
 type Store = {
   counter: number;
   user: {
     username: string;
+    email: string;
+    name: string;
   };
 };
 
@@ -16,31 +19,25 @@ type Actions = {
 
 export const useStore = create<Store & Actions>()(
   devtools(
-    (set, get) => ({
+    immer((set, get) => ({
       counter: 0,
       user: {
         username: "andreeltonsf",
+        email: "andreeltonsf@gmail.com",
+        name: "Andreel Tonsf",
       },
       increment: () =>
-        set((state) => ({ counter: state.counter + 1 }), false, "increment"),
+        set((state) => {
+          state.counter += 1;
+        }),
       decrement: () =>
-        set((state) => ({ counter: state.counter - 1 }), false, "decrement"),
-      setUsername: (username: string) =>
-        set((prevState) => ({
-          user: {
-            /** TODO: Utilização para persistir os dados quenão queremos mudar/ */
-            ...prevState.user,
-            username,
-          },
-        })),
-    }),
-    {
-      name: "project-zustand",
-      storage: createJSONStorage(() => ({
-        getItem: (key) => sessionStorage.getItem(key),
-        setItem: (key, value) => localStorage.setItem(key, value),
-        removeItem: (key) => localStorage.removeItem(key),
-      })),
-    }
+        set((state) => {
+          state.counter -= 1;
+        }),
+      setUsername: (username) =>
+        set((prevState) => {
+          prevState.user.username = username;
+        }),
+    }))
   )
 );
